bbbc01a876e3d2b13f0c9c7dd808e336764139f7
Sakib Noman <skbnmn@gmail.com>
2021-07-19 23:41:55 +0600

    dynamic add

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,44 +1,67 @@
-import React from "react";
-import { Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Form, Row } from "react-bootstrap";
 import DraftCard from "../DraftCard/DraftCard";
 import "./Home.css";
 
-const data = [
-  {
-    title: "First Card",
-    description:
-      "My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r ",
-  },
-  {
-    title: "Second Card",
-    description:
-      "My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you",
-  },
-  {
-    title: "Second Card",
-    description:
-      "My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you",
-  },
-  {
-    title: "Second Card",
-    description:
-      "My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you",
-  },
-  {
-    title: "Second Card",
-    description:
-      "My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you My job is you r My job is you r My job is you r My job is you r My job is you r My job is you r My job is you",
-  },
-];
+interface IDraft {
+  title: string;
+  description: string;
+}
 
 const Home = () => {
+  const [draft, setDraft] = useState<IDraft>({
+    title: "",
+    description: "",
+  });
+
+  const [draftList, setDraftList] = useState<IDraft[]>([]);
+
+  const onClick = () => {
+    setDraftList([...draftList, draft]);
+    setDraft({
+      title: "",
+      description: "",
+    });
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDraft({ ...draft, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
+      <div className="mt-5 input-area">
+        <Form>
+          <Form.Group className="mb-3" controlId="title">
+            <Form.Control
+              className="input-box"
+              name="title"
+              type="text"
+              placeholder="Title"
+              onChange={onChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="description">
+            <Form.Control
+              className="input-box"
+              as="textarea"
+              placeholder="Description"
+              name="description"
+              rows={3}
+              onChange={onChange}
+            />
+          </Form.Group>
+          <Button onClick={onClick} variant="dark">
+            Add To List
+          </Button>
+        </Form>
+      </div>
       <Row className="justify-content-center">
-        {data.map((each) => (
+        {draftList.map((each) => (
           <DraftCard
             title={each.title}
             description={each.description}
+            key={each.title}
           ></DraftCard>
         ))}
       </Row>
